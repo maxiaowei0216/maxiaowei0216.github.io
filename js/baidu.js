7bfe2fdb7cd35934c3fb2590ed4bd1a81dcb165c
Ma Xiaowei <maxiaowei0216@gmail.com>
2021-09-17 08:31:43 +0000

    Site updated: 2021-09-17 08:31:43

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?75c77a06128d0e6c104a661fbbe692bf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
